04b6cf7152e60251d5102a5f3b0f9392f02f71c4
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-01-12 21:09:22 -0800

    Create Permission.php

---
+++ src/terpz710/mineconomy/utils/Permission.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace terpz710\mineconomy\utils;
+
+final class Permission {
+
+    const PERM_ADDFUNDS = "mineconomy.addfunds";
+    const PERM_REMOVEFUNDS = "mineconomy.removefunds";
+    const PERM_SETFUNDS = "mineconomy.setfunds";
+    const PERM_PAYFUNDS = "mineconomy.payfunds";
+    const PERM_MYFUNDS = "mineconomy.myfunds";
+    const PERM_SEEFUNDS = "mineconomy.seefunds";
+
+}
